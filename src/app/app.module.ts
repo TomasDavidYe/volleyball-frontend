6ca8658f1a83c72a4c6417322d11a2e6e4290c45
Tomas Ye <t.ye@sap.com>
2018-11-05 07:00:38 +0100

    starting to implement facebook login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from  '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
@@ -14,38 +14,55 @@ import { PlayersComponent } from './players/players.component';
 import { MatchesComponent } from './matches/matches.component';
 import { StatisticsComponent } from './statistics/statistics.component'
 import { DataService } from './services/data-service';
+import { LoginComponent } from './login/login.component';
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angular-6-social-login'
+import { getAuthServiceConfigs } from './social_login_config'
+import { FacebookModule } from 'ngx-facebook'
 
 registerLocaleData(en);
 
 const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'players', component: PlayersComponent},
-    {path: 'matches', component: MatchesComponent},
-    {path: 'statistics', component: StatisticsComponent},
-    {path: '**', redirectTo: '/home'}
+    { path: 'home', component: HomeComponent },
+    { path: 'players', component: PlayersComponent },
+    { path: 'matches', component: MatchesComponent },
+    { path: 'statistics', component: StatisticsComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '/home' }
 ]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PlayersComponent,
-    MatchesComponent,
-    StatisticsComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    NgZorroAntdModule
-  ],
-  providers: [
-  { provide: NZ_I18N, useValue: en_US }, DataService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        PlayersComponent,
+        MatchesComponent,
+        StatisticsComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        NgZorroAntdModule,
+        SocialLoginModule,
+        FacebookModule.forRoot()
+    ],
+    providers: [
+        {
+            provide: NZ_I18N,
+            useValue: en_US
+        },
+        {
+            provide: AuthServiceConfig,
+            useFactory: getAuthServiceConfigs
+        },
+        DataService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
